dc1aac5fe62841c77b98ba0ff1d9de532ccdf078
KHALID ABDIGANI <almursal45@gmail.com>
2026-07-17 01:12:25 +0300

    New project: Video downloader

---
+++ Video-Express/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors= require("cors")
+require("dotenv").config()
+const app = express()
+
+app.use(cors({
+    origin:process.env.CLIENT_PORT, 
+    credentials:true
+}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+PORT= process.env.PORT
+app.listen(PORT,()=>{
+    console.log("Connected to the server successfully!")
+})
---
+++ Video-Next/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div style={{display:"flex", alignItems:"center", flexDirection:"column", justifyContent:"center", width:"100%", height:"100vh"}}>
+
+      <h1 > VIDEO DOWNLOADER project</h1>
+     
+    </div>
+  );
+}
